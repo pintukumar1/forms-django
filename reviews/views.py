01b871574f1ec8ba6c8fc352c805fd562f15e394
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-08-02 00:47:57 +0530

    added class based view with get and post method

--- reviews/views.py
+++ reviews/views.py
@@ -3,29 +3,49 @@ from django.shortcuts import render
 
 from .models import Review
 from .forms import ReviewForm
-
+from django.views import View
 # Create your views here.
 
-def review(request):
-  if request.method == 'POST':
-    form = ReviewForm(request.POST)
-
-    if form.is_valid() :
-      print(form.cleaned_data)
-      review = Review(
-                user_name=form.cleaned_data["user_name"], 
-                review_text=form.cleaned_data["review_text"],
-                rating=form.cleaned_data["rating"]
-                )
-      review.save()
-      return HttpResponseRedirect("/thank-you")
-
-  else:
+class ReviewView(View):
+  def get(self, request):
     form = ReviewForm()
 
-  return render(request, "reviews/review.html", {
-    "form" : form
-  })
+    return render(request, "reviews/review.html", {
+      "form" : form
+    })
+
+  def post(self, request):
+        form = ReviewForm(request.POST)
+        if form.is_valid() :
+              form.save()
+              return HttpResponseRedirect("/thank-you")
+       
+        return render(request, "reviews/review.html", {
+          "form" : form
+        })
+
+
+# def review(request):
+#   if request.method == 'POST':
+#     form = ReviewForm(request.POST)
+
+#     if form.is_valid() :
+#       form.save()
+#       print(form.cleaned_data)
+#       # review = Review(
+#       #           user_name=form.cleaned_data["user_name"], 
+#       #           review_text=form.cleaned_data["review_text"],
+#       #           rating=form.cleaned_data["rating"]
+#       #           )
+#       # review.save()
+#       return HttpResponseRedirect("/thank-you")
+
+#   else:
+#     form = ReviewForm()
+
+#   return render(request, "reviews/review.html", {
+#     "form" : form
+#   })
 
 
 def thank_you(request):
