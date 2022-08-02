ad77ffa774c3dc2add435a2c6d06703cae6deea2
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-08-02 16:03:57 +0530

    added image in models

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,34 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
-
+from .forms import ProfileForm
+from .models import UserProfile
 # Create your views here.
 
+def store_file(file):
+    with open("temp/image.jpg", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+                        
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
     def post(self, request):
-        pass    
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            # store_file(request.files["image"])
+            saveImage = UserProfile(image = request.FILES["user_image"])
+            saveImage.save()
+            return HttpResponseRedirect("/profiles")
+
+        # image = request.FILES["image"]
+        # print(image)
+        # store_file(image)
+        return render(request, "profiles/create_profile.html", {
+            "form": submitted_form
+        })
